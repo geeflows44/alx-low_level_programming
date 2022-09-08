a50f08319a8f5149f899fb5b402f4ea00b31647f
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2022-09-08 15:39:34 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
-1 #include <stdio.h>
-
-2 /**
-
-3  *main - main block
-
-4  *Return: 0
-
-5  */
-
-6  int main(void)
-
-7  {
-
-8	    puts("\"Programming is like building a multilingual puzzle");
-
-9	    return (0);
-
-10 }
+#include <stdio.h>
+/**
+*main - main block
+*Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
