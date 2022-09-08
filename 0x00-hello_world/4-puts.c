cbabbcb9a898542bcad398375229bd980304fd77
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2022-09-08 13:15:20 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+1 #include <stdio.h>
+2 /**
+3 * main - Entry point
+4 *
+5 * Return: always 0
+6 */
+7 int main(void)
+8 {
+9	puts("\"Programming is like building a multilingual puzzle");
+10	return (0);
+11 }
