42bcaa42cec3a967014f147c0983d3bb77f22343
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2022-09-21 12:48:52 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
 #include "main.h"
-/**
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+/*
 * write your line of code here...
 * Remember:
 * - you are not allowed to use a
@@ -7,15 +14,8 @@
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-int main(void)
-{
-int n;
-int a[5];
-int *p;
-a[2] = 1024;
-p = &n;
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
-_putchar("a[2] = %d\n", a[2]);
+printf("a[2] = %d\n", a[2]);
 return (0);
 }
