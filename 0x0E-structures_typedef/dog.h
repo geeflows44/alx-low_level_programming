9617a4455f25383c9a23ee3f55f5cf72d561fc84
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2022-10-10 11:22:17 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,11 +11,10 @@ int _putchar (char c);
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 }
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
