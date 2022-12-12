b911622adbb5409609a1ca6871341b62dbebb95c
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2022-12-12 08:19:14 -0800

    Added executable file

---
+++ 0x18-dynamic_libraries/strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - get length of string
+ * @s: string
+ * 
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+Footer
+© 2022 GitHub, Inc.
